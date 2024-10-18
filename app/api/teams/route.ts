38a83ba9231daa0d5886c0ac76b7a6848bf2bd50
Boubacar Ballo <bab9755@nyu.edu>
2024-10-18 18:13:23 -0400

    teams api troubleshooting

--- app/api/teams/route.ts
+++ app/api/teams/route.ts
@@ -10,13 +10,17 @@ export async function POST(req: Request){
 
         const userId1 = searchParams.get('userId1');
         const userId2 = searchParams.get('userId2');
-
-        const user1query = {_id: new ObjectId(userId1 || '')}
-        const user2query = {_id: new ObjectId(userId2 || '')}
-
+        console.log(`The retrieved id's are ${userId1} and ${userId2}`)
+        if(!userId1 || !userId2){
+            return Response.json({'Error': 'Error getting the user ids from the params'}, {status: 400})
+        }
+        const user1query = {_id: new ObjectId(userId1)}
+        const user2query = {_id: new ObjectId(userId2)}
+        console.log(`The retrieved queries are ${user1query._id} and ${user2query._id}`)
         const userCollection = await getDB('leetbuds', 'users');
         const user1 = await userCollection?.findOne(user1query)
         const user2 = await userCollection?.findOne(user2query)
+        console.log(`${user1} and ${user2} are being processed`)
 
         if (!user1 || !user2){
             return Response.json({'Error': 'Error getting the users'}, {status: 400})
@@ -28,11 +32,22 @@ export async function POST(req: Request){
             const team = await teamCollection?.insertOne({teamMembers: [userId1, userId2]})
             const user1 = await userCollection?.updateOne(user1query, {$set: {teamId: team?.insertedId}})
             const user2 = await userCollection?.updateOne(user2query, {$set: {teamId: team?.insertedId}})
-            return Response.json({'message': 'Team created successfully'})
+            return Response.json({'message': 'Team created successfully for user 1 and user 2'})
         }
-        await addFriend(userId1, userId2);
+        if (user2?.teamId){
+            return Response.json({'Error': 'User is already in a team'}, {status: 400})
+        }
+        if (user1?.teamId){
+            const teamId = user1?.teamId
+            const teamCollection =  await getDB('leetbuds', 'teams');
+            const team = await teamCollection?.findOne({_id: new ObjectId(teamId)})
+            team?.teamMembers?.push(userId2)
+            const result = await teamCollection?.updateOne({_id: new ObjectId(teamId)}, {$set: {teamMembers: team?.teamMembers}})
+            const user2 = await userCollection?.updateOne(user2query, {$set: {teamId: teamId}})
+            return Response.json({'message': 'Friend added successfully'})
+        }
         return Response.json({'message': 'Friend added successfully'})
     }catch (error){
-        return Response.json({'Error': 'Error getting the users'}, {status: 400})
+        return Response.json({'Error': `Error adding the friends: ${error}`}, {status: 400})
     }
 }
