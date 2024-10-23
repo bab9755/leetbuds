0c118613b41fe21836ec19cbcbec604434eb1939
Boubacar Ballo <bab9755@nyu.edu>
2024-10-22 21:07:58 -0400

    create friend request api

--- app/api/users/[user]/route.ts
+++ app/api/users/[user]/route.ts
@@ -28,3 +28,32 @@ export async function GET(req: Request, context: {params: any}) {
         return Response.json({'Error': `Error getting the user: ${error}`})
     }
 }
+
+
+export async function POST(req: Request, context: {params: any}) {
+    const userId = context.params.user
+    try{
+        const {searchParams} = new URL(req.url);
+        const friendId = searchParams.get('friendId');
+        if(!friendId) return Response.json({'Error': 'Error getting the friend id from the params'})
+        const userCollection = await getDB('leetbuds', 'users');
+        const user = await userCollection?.findOne({_id: new ObjectId(userId)})
+        const friend = await userCollection?.findOne({_id: new ObjectId(friendId)})
+        if(!user){
+            return Response.json({'Error': 'Error getting the user from the database'}, {status: 400})
+        }
+        if(!friend){
+            return Response.json({'Error': 'Error getting the friend from the database'}, {status: 400})
+        }
+        const friendRequests = friend?.friendRequests || []
+        friendRequests.push(userId)
+        await userCollection?.updateOne({_id: new ObjectId(friendId)}, {$set: {friendRequests: friendRequests}})
+
+        return Response.json({
+            message: 'sucessfuly sent a friend request to the other user'
+        },
+        {status: 200})
+    }catch(error){
+        return Response.json({'Error': `Error sending a friend request: ${error}`})
+    }
+}
